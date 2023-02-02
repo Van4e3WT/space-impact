546a1c147c3926359fb60b0790f1de1c887c50a0
Ivan Gladyshev <ivan.gladyshev@metalamp.io>
2023-02-02 10:27:01 +0500

    create basic file struct

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,7 @@
-import './App.scss';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit
-          <code>src/App.tsx</code>
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    Application starts here
+  </div>
+);
 
 export default App;

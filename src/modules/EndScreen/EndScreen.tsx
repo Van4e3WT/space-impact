ec8b341af8a275c0573a94e04e487c092553285c
Ivan Gladyshev <ivan.gladyshev@metalamp.io>
2023-04-26 12:04:10 +0500

    add final score on EndScreen

--- src/modules/EndScreen/EndScreen.tsx
+++ src/modules/EndScreen/EndScreen.tsx
@@ -5,6 +5,13 @@ export const EndScreen = () => (
   <div className={S['wrapper']}>
     <div className={S['container']}>
       <h1 className={S['title']}>Game over</h1>
+      <span className={S['score']}>
+        Your score:
+        {' '}
+        <span className={S['highlight']}>{store.game.playerScore}</span>
+        {' '}
+        points
+      </span>
       <button
         type="button"
         onClick={() => store.game.startGame()}
